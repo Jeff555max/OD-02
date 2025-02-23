e9266b92ddd055abae16d993c7da3bda65e95875
Jeff555max <vadik98765@gmail.com>
2025-02-23 17:03:21 +0500

    2

--- queue.py
+++ queue.py
@@ -1,32 +1,30 @@
+# Очередь (Queue) — это структура данных, работающая по принципу "первый пришел — первый ушел" (First In, First Out)
 class Queue:
-   def __init__(self):
-       self.items = []
+    def __init__(self):
+        self.items = []
 
-   def is_empty(self):
-       return self.items == []
+    def is_empty(self):
+        return self.items == []
 
-   def enqueue(self, item):
-       self.items.insert(0, item)
+    # добавления элемента в конец очереди, те. вначало списка
+    def enqueue(self, item):
+        self.items.insert(0, item)
 
-   def dequeue(self):
-       return self.items.pop()
+    # Удалять будем первый элемент очереди, то есть последний элемент списка
+    def dequeue(self):
+        return self.items.pop()
 
-   def size(self):
-       return len(self.items)
+    # Подсчет количество элементов находится в очереди
+    def size(self):
+        return len(self.items)
+
 
 queue = Queue()
-
 print(queue.is_empty())
-
-queue.enqueue("действие 1")
-queue.enqueue("действие 2")
-queue.enqueue("действие 3")
-queue.enqueue("действие 4")
-
-# ["действие 4", "действие 3", "действие 2", "действие 1"]
-
-print(queue.is_empty())
+queue.enqueue('Act1')
+queue.enqueue('Act2')
+queue.enqueue('Act3')
 print(queue.size())
 print(queue.dequeue())
+print(queue.is_empty())
 print(queue.size())
-
